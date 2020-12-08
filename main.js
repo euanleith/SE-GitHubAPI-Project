3a6341494f70f4d405cb8690cceacc48d704de0f
Euan Leith <leithe@tcd.ie>
2020-12-08 20:47:00 +0000

    made http request async

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-function queryRepo() {
+async function queryRepo() {
     const username = document.getElementById('username').value;
     const repo = document.getElementById('repo').value;
     const token = document.getElementById('token').value;
-    const request = new XMLHttpRequest();
-    request.open('GET', 'https://api.github.com/repos/' + username + '/' + repo, false);
-    request.setRequestHeader('Authorization', 'token ' + token);
-    request.send();
-    document.write(request.responseText);
+    if (username === '' || token === '') {
+        alert("user/token not entered")
+        return false;
+    }
+
+    let url;
+    if (repo === '')
+        url = 'https://api.github.com/users/' + username; // get user
+    else
+        url = 'https://api.github.com/repos/' + username + '/' + repo // get repo
+
+    let result = await fetch(url, {
+        method: 'GET',
+        headers: {
+            Authorization: 'token ' + token
+        }
+    }).then(result=>result.json());
+    document.write(JSON.stringify(result));
+    return true;
 }
